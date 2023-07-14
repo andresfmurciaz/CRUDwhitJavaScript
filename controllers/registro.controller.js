63f8f565919787fc0c69646960d319f68325f3a8
andresf.murciaz <andresf.murciaz@gmail.com>
2023-07-14 12:37:54 -0500

    post

--- controllers/registro.controller.js
+++ controllers/registro.controller.js
@@ -1,9 +1,19 @@
+//captura informacion
+
+import { clientService } from "../service/client-service.js";
+
 const formulario = document.querySelector("[data-form]");
 
 formulario.addEventListener("submit",(evento)=>{
-    evento.preventDefault();
-    const nombre = document.querySelector("[data-nombre]").value;
-    const email = document.querySelector("[data-email]").value;
-    console.log(nombre,email);
+        evento.preventDefault();
+        const nombre = document.querySelector("[data-nombre]").value;
+        const email = document.querySelector("[data-email]").value;
+        console.log(nombre,email);
+// envia al post 
+        clientService.crearCliente(nombre,email).then(respueta=>{
 
+        console.log(respueta);
+        }).catch(err=>console.log(respueta));
 })
+
+
--- service/client-service.js
+++ service/client-service.js
@@ -1,8 +1,23 @@
-// hacer comunicacion con servidor
+// hacer comunicacion con servidor con los datos que le llegan
 
  const listaClientes = () => 
     fetch(" http://localhost:3000/perfil").then((Response)=> Response.json());
+  
     
+const crearCliente = (nombre,email)=>
+{
+    return fetch("http://localhost:3000/perfil",
+    {
+    method:"POST",
+    headers:
+    {
+        "content-Type":"application/json",
+    },body:JSON.stringify({nombre,email,id:uuid.v4()})
+    });
+
+}
+
 export const clientService = {
     listaClientes,
+    crearCliente
 };
