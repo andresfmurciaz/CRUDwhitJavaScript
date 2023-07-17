917eefe2fc4ca1c194af253fc2a3e78d8f0b1da6
Andres Felipe Murcia Zapata <101027787+andresfmurciaz@users.noreply.github.com>
2023-07-17 13:19:01 -0500

    updat montran en input

--- controllers/actualizar.controller.js
+++ controllers/actualizar.controller.js
@@ -3,7 +3,20 @@ import { clientService } from "../service/client-service.js"
 const obtenerInformacion = () =>{
     const url = new URL(window.location)
     const id = url.searchParams.get("id")
-    clientService.detalleCliente(id).then((perfil)=>console.log(perfil));    
+
+    const nombre = document.querySelector("[data-nombre]") 
+
+    const email = document.querySelector("[data-email]")
+
+    console.log(nombre,email)
+
+    clientService.detalleCliente(id).then((perfil)=>{
+
+        nombre.value= perfil.nombre;
+        email.value=perfil.email;
+    });  
+    
+    
 }
 
 obtenerInformacion();
