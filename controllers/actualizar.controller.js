555f1f5b415692e8413d2e87e4bec14b58d413ce
Andres Felipe Murcia Zapata <101027787+andresfmurciaz@users.noreply.github.com>
2023-07-17 15:19:52 -0500

    funciones asincronas

--- controllers/actualizar.controller.js
+++ controllers/actualizar.controller.js
@@ -2,7 +2,7 @@ import { clientService } from "../service/client-service.js"
 
 const formulario = document.querySelector("[data-form]")
 
-const obtenerInformacion = () =>{
+const obtenerInformacion = async () =>{
     const url = new URL(window.location)
     const id = url.searchParams.get("id")
 
@@ -14,12 +14,11 @@ const obtenerInformacion = () =>{
 
     const nombre = document.querySelector("[data-nombre]") 
     const email = document.querySelector("[data-email]")
-
-    clientService.detalleCliente(id).then((perfil)=>{
+    const perfil = await clientService.detalleCliente(id)
 
         nombre.value= perfil.nombre;
         email.value=perfil.email;
-    });  
+      
     
     
 }
@@ -33,7 +32,7 @@ formulario.addEventListener("submit",(evento)=>{
         const id = url.searchParams.get("id")
         const nombre = document.querySelector("[data-nombre]").value
         const email = document.querySelector("[data-email]").value
-        console.log(nombre,email);
+       
         clientService.actualizarCliente(nombre,email,id).then(()=>{window.location.href="/screens/edicion_concluida.html"})
 
 
